420f07fd22a65e37127edcb98a616388fabe7513
RUMENRUSCHEV <61242729+RUMENRUSCHEV@users.noreply.github.com>
2020-02-19 18:33:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int r = 0;
+            Console.WriteLine("Type some number:");
+            input = Console.ReadLine();
+            for (int i = 0; i <= int.Parse(input); i++)
+            {
+                r = r + i;
+            }
+            Console.WriteLine(r);
+        }   
+    }
+}
